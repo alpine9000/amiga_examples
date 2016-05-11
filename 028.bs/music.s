c2df9f91ffbfd9fac957fd80e1eedcf0c3b2cadc
alpine9000 <alex.g.brown@gmail.com>
2016-05-11 17:35:38 +1000

    load music on demand

--- 028.bs/music.s
+++ 028.bs/music.s
@@ -1,12 +1,28 @@
 	xdef 	StartMusic
-	
+
 StartMusic:
+	;; a0 - destination address
+	;; a1 - start address
+	;; d0 - size	
+
+	lea	module,a0
+	lea	diskmodule,a1
+	move.l	#enddiskmodule-diskmodule,d0
+        jsr	LoadDiskData
 	lea     module,a0
         sub.l   a1,a1
         sub.l   a2,a2
         moveq   #0,d0
         jsr     P61_Init
 	rts
-	
-module:
+
+	section	.bss
+module
+	ds.b	enddiskmodule-diskmodule
+
+	section	.noload
+	cnop	0,512	
+diskmodule:
 	incbin	"assets/P61.placeholder"
+	cnop	0,512
+enddiskmodule:	
